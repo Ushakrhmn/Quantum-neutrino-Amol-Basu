a3606720d529d579a2118b1170798ca52ab49d84
Theo <xyz.yu@mail.utoronto.ca>
2025-05-20 16:16:01 +0200

    Add utility to convert Qiskit QuantumCircuit to Quimb circuit

---
+++ utils/CircuitConvert.py
@@ -0,0 +1,51 @@
+import qiskit as qk
+from qiskit import QuantumCircuit
+import quimb as qu
+import quimb.tensor as qtn
+
+def qiskit_to_quimb(qc):
+    """
+    Converts a qiskit QuantumCircuit object
+    to a equivalent quimb circuit object
+
+    Currently only supports 1 and 2 qubit gates of specific basis:
+
+    [CZ, RZ, SX, X]
+
+    :param qc: qiskit QuantumCircuit
+        The circuit to be converted
+    :return: quimb Circuit
+
+    :raise NotImplementedError: if circuit contains >2 qubit gates.
+    """
+    qu_circuit = qtn.Circuit(qc.num_qubits)
+
+    op1_dict = {
+        'x': 'X',
+        'rz': 'RZ',
+        'sx': 'SX'
+    }
+
+    op2_dict = {
+        'cz': 'CZ'
+    }
+    for instru in qc.data: # expand into each instruction
+        op = instru.operation
+        if op.num_qubits == 1: # this is a single qubit gate
+            if op.name == 'x' or op.name == 'sx':
+                qu_circuit.apply_gate(op1_dict[op.name], instru.qubits[0]._index)
+            elif op.name == 'rz':
+                print(instru.params)
+                print(instru.qubits)
+                qu_circuit.apply_gate(op1_dict[op.name], instru.params[0], instru.qubits[0]._index)
+            else:
+                raise NotImplementedError("Currently only supports X, RZ, SX gates")
+        elif op.num_qubits == 2: # this is a two qubit gate
+            if op.name == 'cz':
+                qu_circuit.apply_gate(op2_dict[op.name], instru.qubits[0]._index, instru.qubits[1]._index)
+            else:
+                raise NotImplementedError("Currently only supports CZ gates")
+        else:
+            raise NotImplementedError("Currently only supports 1 and 2 qubit gates")
+    
+    return qu_circuit
