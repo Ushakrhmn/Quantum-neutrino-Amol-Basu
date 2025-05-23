5c1f9aab5abb868d4941171a6c52ee1b998c50e8
Xin Yue Yu <theo1701@gra-login1.graham.sharcnet>
2025-05-23 06:51:46 -0400

    add symengine dependency for graham to job sh

--- SBN/tensorjob.sh
+++ SBN/tensorjob.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #SBATCH --account=def-nilic 
-#SBATCH --mem=16000M               # memory per node
-#SBATCH --time=0-03:00:00
+#SBATCH --mem=32G               # memory per node
+#SBATCH --time=0-24:00:00
 #SBATCH --mail-user=xyz.yu@mail.utoronto.ca
 #SBATCH --mail-type=ALL
+#SBATCH --gpus-per-node=1
+
+module load StdEnv/2023 gcc python/3.11 symengine/0.11.2
+
+nvidia-smi
+
 source ~/ENV/bin/activate
 
-python SBN/SNB_3flavour_20-neutrino_2407_tensor.py
+python SNB_3flavour_20-neutrino_2407_tensor.py
