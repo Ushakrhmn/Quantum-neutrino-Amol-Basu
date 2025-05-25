4b3ab1e709e3266c34d9102e6dc425f5791433a9
Theo <xyz.yu@mail.utoronto.ca>
2025-05-25 19:15:33 +0200

    More refactoring for decorenorm runs

--- SBN/transpile.py
+++ SBN/transpile.py
@@ -1,10 +1,15 @@
-def produce_transpile_args(config=None, opt_lvl = 3, initial_layout=None, approximation_degree=1.0):
+import json
+
+def produce_transpile_args(config=None, path = None):
+    with open(path, 'r') as f:
+        transpile_config = json.load(f)
+
     transpile_args = {
-        'optimization_level': opt_lvl
+        'optimization_level': transpile_config.get('optimization_level', 3),
     }
 
     transpile_args['basis_gates'] = config.supported_instructions if config is not None and hasattr(config, 'supported_instructions') else None
-    transpile_args['initial_layout'] = initial_layout
-    transpile_args['approximation_degree'] = approximation_degree
+    transpile_args['initial_layout'] = transpile_config.get('initial_layout', None)
+    transpile_args['approximation_degree'] = transpile_config.get('approximation_degree', 1)
         
     return transpile_args
