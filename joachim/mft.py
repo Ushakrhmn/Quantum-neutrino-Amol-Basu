38030863701c4173e84b79cdbd2abca611219bc8
Theo <xyz.yu@mail.utoronto.ca>
2025-07-08 14:11:01 +0200

    Add refactored mft script

---
+++ joachim/mft.py
@@ -0,0 +1,224 @@
+"""
+Functions taken from bipolar-oscillations.ipynb, Raffelt–Sigl Polarization-Vector Approach to Collective Oscillations
+"""
+
+import sys, os
+import matplotlib
+import numpy as np
+import scipy.integrate as integ
+
+# Unit conversion and physical constants
+class my_units:
+    # Energy and mass
+    eV     = 1.
+    keV    = 1.e3
+    MeV    = 1.e6
+    GeV    = 1.e9
+    TeV    = 1.e12
+    PeV    = 1.e15
+    kg     = 5.62e35*eV
+    grams  = 0.001*kg
+    Kelvin = 8.617333262e-5       # PDG 2022
+    Joule  = 1/1.602176634e-19*eV # PDG 2022 [exact]
+    erg    = 1e-7*Joule
+    
+    # Length and time
+    m     = 5.076e6
+    meter = m
+    km    = 1000*m
+    cm    = 0.01*m
+    nm    = 1.e-9*m
+    fm    = 1.e-15*m
+    AU    = 1.4960e11*m
+    pc    = 30.857e15*m
+    kpc   = 1.e3*pc
+    Mpc   = 1.e6*pc
+    Gpc   = 1.e9*pc
+    ly    = 9460730472580800*m  # light year
+    sec   = 1.523e15
+    hours = 3600*sec
+    days  = 24*hours
+    yrs   = 365*days
+    Hz    = 1./sec
+    kHz   = 1.e3*Hz
+    MHz   = 1.e6*Hz
+    GHz   = 1.e9*Hz
+    
+    barn  = 1.e-24*cm**2
+    pb    = 1.e-36*cm**2
+    fb    = 1.e-39*cm**2
+    
+    # Various astrophysical constants
+    GN    = 6.708e-39/1e18  # eV^-2, Newton's constant
+    MPl   = 1.22093e19*GeV   # Planck mass, PDG 2013
+    Msun  = 1.989e30*kg
+    Rsun  = 6.9551e8*meter
+    
+    # cosmology
+    h        = 0.674                         # according to Planck 2018
+    H0       = h * 100. * km / sec/ Mpc      # Hubble parameter
+    rho_c0   = 3. * H0**2/(8. * np.pi * GN)  # critical density today, Kolb Turner eq. (3.14)
+    Omega_dm = 0.12 / h**2                   # dark matter density, Planck 2018
+    
+    # particle physics
+    alpha_em = (1./137.035999139)            # electromagnetic fine structure constant (PDG 2018)
+    GF       = 1.166378e-5 / GeV**2          # Fermi constant (PDG 2022)
+    s2thW    = 1.03232 * 0.23121             # sin^2 \theta_W(M_Z) from PDG 2022; \kappa from https://arxiv.org/abs/1608.02671
+    m_e      = 0.5109989461 * MeV            # electron mass (PDG 2018)
+    m_mu     = 105.6583745 * MeV             # muon mass (PDG 2018)
+    m_tau    = 1776.86 * MeV                 # tau mass (PDG 2018)
+    m_p      = 938.2720813 * MeV             # proton mass (PDG 2018)
+    m_n      = 939.565413 * MeV              # neutron mass (PDG 2018)
+    m_u      = 931.4940954 * MeV             # atomic mass unit (PDG 2018)
+    m_pi0    = 134.9768 * MeV                # neutral pion mass (PDG 2020)
+    tau_mu   = 2.1969811e-6 * sec            # muon lifetime (PDG 2018)
+    gA       = 1.26                          # axial coupling constant of the proton
+    
+    # PDG codes
+    pdg_nu_e      = 12
+    pdg_nu_mu     = 14
+    pdg_nu_tau    = 16
+    pdg_e         = 11
+    pdg_mu        = 13
+    pdg_tau       = 15
+    pdg_gamma     = 22
+    pdg_pip       = 211
+    pdg_pi0       = 111
+    pdg_KL        = 130
+    pdg_Kp        = 321
+    pdg_Dp        = 411
+    pdg_D0        = 421
+    pdg_Dp2s_2460 = 415
+    pdg_D02s_2460 = 425
+    pdg_Dsp       = 431
+    pdg_Dsps      = 433
+    pdg_Ds2s_2573 = 435
+    
+u = my_units()
+
+# Pauli matrices
+sigma_0 = np.array([[1, 0],  [0,  1]])
+sigma_1 = np.array([[0, 1],  [1,  0]])
+sigma_2 = np.array([[0,-1j], [1j, 0]])
+sigma_3 = np.array([[1, 0],  [0, -1]])
+
+
+def P_osc_RS(t_table, theta, omega, lam, J, initial_flavors=None, alpha=None):
+    """Compute an oscillation probability for collective oscillations
+       in the Raffelt-Sigl 2-flavor polarization vector approach.
+       
+       Arguments:
+           t_table: list of time steps
+           theta:   vacuum mixing angle
+           omega:   vector of vacuum oscillation frequencies for each mode
+           lam:     strength of ordinary matter effect
+           J:       matrix of coupling strengths between modes
+           initial_flavors: an array of length n_modes indicating the initial flavor of
+                    each neutrino ('e', 'x', or 'a'). If any neutrino has flavor 'a',
+                    an angle alpha must be specified.
+           alpha:   initial superposition for neutrinos in flavor 'a'
+    """
+    
+    n_modes = J.shape[0]
+    
+    # definition of the vectors B, D, L, P, H, e3m in the notation of
+    # https://arxiv.org/abs/1001.2799 (Duan, Fuller Qian)
+    u   = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+    b   = 0.5 * np.diag([-1,1])
+    b   = u @ b @ u.T
+    l   = np.diag([1,0])
+    
+    Ee  = np.squeeze(np.array([0, 0,  1.]))
+    Emu = np.squeeze(np.array([0, 0, -1.]))
+    B   = np.real(np.array([np.trace(b@sigma_1), np.trace(b@sigma_2), np.trace(b@sigma_3)]))
+    L   = np.real(np.array([np.trace(l@sigma_1), np.trace(l@sigma_2), np.trace(l@sigma_3)]))
+    
+    ini_state = np.where(omega>0, Ee[:,None], Emu[:,None])
+    if initial_flavors is not None:
+        for f in initial_flavors:
+            if f not in ['e','x','mu','a']:
+                raise ValueError(f'Unknown flavor: {f}')
+            if f == 'a' and alpha is None:
+                raise ValueError('Initial superposition for neutrinos in flavor "a" not specified')
+        for ifi in range(len(initial_flavors)):
+            if initial_flavors[ifi] == 'e':
+                ini_state[:,ifi] = Ee[:]
+            elif initial_flavors[ifi] == 'x':
+                ini_state[:,ifi] = Emu[:]
+            elif initial_flavors[ifi] == 'mu':
+                ini_state[:,ifi] = Emu[:]
+            elif initial_flavors[ifi] == 'a':
+                # FIXME I'm not sure this is correct
+                ini_state[:,ifi] = np.array([np.sin(2*alpha), 0, np.cos(2*alpha)])[:]
+
+        # ini_state[:,initial_flavors=='e']  = Ee[:,None]
+        # ini_state[:,initial_flavors=='x']  = Emu[:,None]
+        # ini_state[:,initial_flavors=='mu'] = Emu[:,None]
+        # ini_state[:,initial_flavors=='a']  = np.array([np.sin(2*alpha), 0, np.cos(2*alpha)])[:,None]
+        # FIXME I'm not sure this is correct
+        
+    def f(t, P):
+        """right-hand side of the evolution equation for the polarization vector P at time t."""
+        
+        res = np.zeros(3*n_modes)
+        PP  = np.reshape(P, (n_modes,3)).T
+        for k in range(n_modes):
+            # if k >= n_modes // 2:
+            #     res[3*k:3*k+3] = np.cross(-omega[k]*B + lam*L - PP@J[k,:], PP[:,k])
+            # else:
+            #     res[3*k:3*k+3] = np.cross(omega[k]*B + lam*L + PP@J[k,:], PP[:,k])
+
+            # per paper
+            res[3*k:3*k+3] = np.cross(omega[k]*B + lam*L + PP@J[k,:], PP[:,k])
+        return res
+
+    return integ.solve_ivp(f, (t_table[0],t_table[-1]), ini_state.T.flatten(), t_eval=t_table)
+
+def P_osc_RS_bipolar(t_table, theta, omega, lam, J, eps=0.1):
+    """Compute an oscillation probability for collective oscillations
+       in the Raffelt-Sigl 2-flavor polarization vector approach for
+       a bipolar system.
+       
+       Arguments:
+           t_table: list of time steps
+           theta:   vacuum mixing angle
+           omega:   vector of vacuum oscillation frequencies for each mode
+           lam:     strength of ordinary matter effect
+           J:       matrix of coupling strengths between modes
+           eps:     asymmetry between neutrinos and anti-neutrinos"""
+    
+    n_modes = J.shape[0]
+    
+    # definition of the vectors B, D, L, P, H, e3m in the notation of
+    # https://arxiv.org/abs/1001.2799 (Duan, Fuller, Qian)
+    u   = np.array([[np.cos(theta), np.sin(theta)], [-np.sin(theta), np.cos(theta)]])
+    b   = 0.5 * np.diag([-1,1])
+    b   = u @ b @ u.T
+    l   = np.diag([1,0])
+    
+    Ee  = np.array([0, 0,  1+eps])
+    Emu = np.array([0, 0, -1+eps])
+    B   = np.array([np.trace(b@sigma_1), np.trace(b@sigma_2), np.trace(b@sigma_3)])
+    L   = np.array([np.trace(l@sigma_1), np.trace(l@sigma_2), np.trace(l@sigma_3)])
+    
+    def f(t, P):
+        """right-hand side of the evolution equation for the polarization vector P at time t."""
+        
+        res = (0+0j)*np.zeros(3*n_modes)
+        PP  = np.reshape(P, (n_modes,3)).T
+        for k in range(n_modes):
+            s  = 1 if k < n_modes//2 else -1
+            res[3*k:3*k+3] = np.cross(s*omega[k]*B + lam*L + PP@J[k,:], PP[:,k])
+        return res
+            
+    E0 = np.concatenate((np.tile(Ee,n_modes//2), np.tile(Emu,n_modes//2)))
+    return integ.solve_ivp(f, (t_table[0],t_table[-1]), E0, t_eval=t_table)    
+
+def U_self_int(theta):
+   """the exponential of the neutrino interaction Hamiltonian,
+      see Friedland & Lunardini, https://arxiv.org/abs/hep-ph/0304055
+   """
+   return np.array([[np.exp(-1j*theta), 0,                          0,                          0],
+                  [0,                 0.5*( 1+np.exp(-1j*theta)), 0.5*(-1+np.exp(-1j*theta)), 0],
+                  [0,                 0.5*(-1+np.exp(-1j*theta)), 0.5*( 1+np.exp(-1j*theta)), 0],
+                  [0,                 0,                          0,                          np.exp(-1j*theta)]])
