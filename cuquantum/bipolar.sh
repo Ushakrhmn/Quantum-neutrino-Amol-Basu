b087d9e86402f631b8424c8b8d2d309ce50a87ff
Theo <xinyue.yu@cern.ch>
2025-07-28 11:09:09 -0400

    update job script.

---
+++ cuquantum/bipolar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH --account=def-nilic 
+#SBATCH --mem=64G               # memory per node
+#SBATCH --time=1-00:00:00
+#SBATCH --gpus-per-node=1
+
+source env.sh
+
+nvidia-smi
+
+python cuquantum_nubarnubar.py "$@"
