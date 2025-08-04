5dd86ff049d6f7650432f7b823ca59ebfc92fd82
theo <xinyue.yu@cern.ch>
2025-08-04 04:36:07 -0700

    Replace ENV path with local path.

--- env.sh
+++ env.sh
@@ -3,6 +3,6 @@ module load gcc symengine/0.11.2 boost
 module load python/3.11
 module load cuquantum
 
-source ~/QNova/ENV/bin/activate
+source ./ENV/bin/activate
 
 echo "Succesfully loaded necessary modules and activated virtual env."
